ad84286904f0af8c7ea1c0bb4e4ee3e7a53bcf23
Christian Gomez <chr8993@gmail.com>
2016-12-07 15:11:35 -0600

    Fixes

--- src/oauth.amazon.js
+++ src/oauth.amazon.js
@@ -27,7 +27,7 @@
           }
           var browserRef = window.cordova.InAppBrowser.open("https://www.amazon.com/ap/oa?client_id=" + clientId + "&redirect_uri=" + redirect_uri + "&response_type=code&scope=profile", "_blank", "location=no,clearsessioncache=yes,clearcache=yes");
           browserRef.addEventListener("loadstart", function(event) {
-            if((event.url).indexOf(redirect_uri) === 0) {
+            if((!!(event.url).match(redirect_uri))) {
               var requestToken = (event.url).split("code=")[1];
               $http({method: "post", headers: {'Content-Type': 'application/x-www-form-urlencoded'}, url: "https://api.amazon.com/auth/o2/token", data: "client_id=" + clientId + "&client_secret=" + clientSecret + "&redirect_uri=" + redirect_uri + "&grant_type=Authorization_code" + "&code=" + requestToken })
                 .success(function(data) {
